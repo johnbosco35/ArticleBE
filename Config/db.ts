c0e92ff561caa1c5d4f1f80934e90814e65617c5
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-09-03 01:34:46 +0100

    added

--- Config/db.ts
+++ Config/db.ts
@@ -3,10 +3,6 @@
 import mongoose from "mongoose";
 import { environmentVariable } from "../env/environmemt";
 
-
-// const Db_String: string = process.env.URL2!;
-// const Db_String: string = process.env.URL2 || "mongodb://localhost:27017/DB";
-// const Db_String = "mongodb://localhost/ArticleDB";
 const Db_String:any = environmentVariable.database
 
 export const DBconnect = async () => {
